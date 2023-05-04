05d86de68f5b716c9487824fa610004d2fe7b6a6
shwetaproject <shweta1talmale@gmail.com>
2023-05-04 11:22:51 +0530

    Project first setup

--- Speedyy/ViewController.swift
+++ Speedyy/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "View controller"
+        view.backgroundColor = .systemRed
+
     }
 
 
